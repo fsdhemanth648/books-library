6ade20810a86a7ac2af13a8c02d9e32d68304c51
fsdhemanth648 <fsdhemanth648@gmail.com>
2024-10-22 11:03:48 +0530

    BLF: customizing fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navy': '#0D0842',
+        'magenta': '#FF5841'
+      },
+      fontFamily: {
+        'primary': ["Montserrat", "sans-serif"],
+        'secondary': ["Nunito", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
